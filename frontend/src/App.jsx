07ee7ef80df2c3bdc48a42b34f63f9023c8fffa8
ANIMESH <animesh.2403414101@vcet.edu.in>
2026-06-18 21:42:57 +0530

    making backgroound image responsive and adding a blur effect to the navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react'
-
+import { ReactLenis } from 'lenis/react';
+import { motion } from 'framer-motion';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-      <h1>Todo List</h1>
-      
-    </>
+    <ReactLenis root>
+      <div classname="first-bg">
+        <section ref={scene1}className="section1">
+        <motion.div 
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5, type: "spring" }}
+        >
+          Hello World!
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          Todo List
+        </motion.div>
+        </section>
+      </div>
+    </ReactLenis>
   )
 }
 
